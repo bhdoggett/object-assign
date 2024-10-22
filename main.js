506f69b54ce44c642a056e5072059b075dc1c03b
Ben Doggett <bdoggett@gmail.com>
2024-10-22 16:03:48 -0400

    Remove non-object argument from function to remove erro message from the console.

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const source1 = { d: 7, g: 18 };
 const source2 = { h: 6, i: 72, j: 89, l: 3456 };
 const source3 = { m: 345, o: 36 };
 
-const returnedTarget = compose(target, source1, source2, source3, 34);
+const returnedTarget = compose(target, source1, source2, source3);
 
 console.log(target); // {d: 7, e: 10, f: 22, g: 18, h: 6, i: 72, j: 89, l: 3456, m: 345, o: 36}
 console.log(returnedTarget === target); // true
